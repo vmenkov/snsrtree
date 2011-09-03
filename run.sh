e29983c20653b42dcf1f7b0765e9cdf77c1c53aa
vmenkov <vmenkov@a2928b8e-b700-11de-a66b-f3ad5cb19abf>
2011-09-03 19:28:37 +0000

    jar file manifest

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ set b=../batik-1.7
 #-- batik.jar is a file that just contains the classpath listing all the other
 #-- jars. So we only need this one
 
-set cp=${b}/batik.jar:classes
+set cp=${b}/batik.jar:lib/dndo.jar
 
 
 java -cp $cp -Xmx512m dd.gui.DDGUI $1
